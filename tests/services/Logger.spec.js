6d307f9b71fd1917aa8e82c738f120ad5225c3db
marnits <m.nitschke@clearcode.cc>
2015-02-18 16:16:41 +0100

    describtion of logger testing

--- tests/services/Logger.spec.js
+++ tests/services/Logger.spec.js
@@ -4,40 +4,78 @@ describe('Unit: Logger', function () {
     var loggerService,
         loggerProvider;
 
+    /**
+     * First mocked handler
+     *
+     * @param level
+     * @param message
+     */
+
     var test_1 = function(level, message){
         console.log(level+': '+message);
     };
+
+    /**
+     * Second mocked handler
+     *
+     * @param level
+     * @param message
+     */
 
     var test_2 = function(level, message){
         console.error(message+', '+message);
     };
 
-    var test_3 = 5;
+    /**
+     * Injecting and creating mocked service and provider objects
+     */
 
     beforeEach(inject(['angular-cc-logger.Logger', 'angular-cc-logger.$logger', function (Logger, provider) {
         loggerService = Logger;
         loggerProvider = provider;
     }]));
 
+    /**
+     * Tests
+     */
+
     it('should be defined', shouldBeDefined);
+
+    /**
+     * Checking if mocked objects are defined
+     */
 
     function shouldBeDefined(){
         expect(loggerService).toBeDefined();
         expect(loggerProvider).toBeDefined();
     }
 
+    /**
+     * Tests for Logger service
+     */
+
     describe('testing each of Logger function', function(){
+
+        /**
+         * Filling handlers table with two mocked functions
+         */
         beforeEach(function () {
             loggerProvider.pushHandler(test_1);
             loggerProvider.pushHandler(test_2);
         });
 
+        /**
+         * Tests
+         */
         it('should be defined (handler)', shouldBeDefinedHandler);
         it('should run logger error', shouldRunLoggerError);
         it('should run logger warning', shouldRunLoggerWarn);
         it('should run logger notice', shouldRunLoggerNotice);
         it('should run logger info', shouldRunLoggerInfo);
 
+        /**
+         * Checking if handlers are defined and if are functions
+         */
         function shouldBeDefinedHandler(){
             for(var i in loggerProvider.handlers){
                 expect(loggerProvider.handlers[i]).toBeDefined();
@@ -45,18 +83,30 @@ describe('Unit: Logger', function () {
             }
         }
 
+        /**
+         * Running logger.error function
+         */
         function shouldRunLoggerError(){
             loggerService.error('error');
         }
 
+        /**
+         * Running logger.warn function
+         */
         function shouldRunLoggerWarn(){
             loggerService.warn('warn');
         }
 
+        /**
+         * Running logger.notice function
+         */
         function shouldRunLoggerNotice(){
             loggerService.notice('notice');
         }
 
+        /**
+         * Running logger.info function
+         */
         function shouldRunLoggerInfo(){
             loggerService.info('info');
         }
