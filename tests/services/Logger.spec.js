7c1dd9cc2e97709d588a7562f966dc30460fd49c
marnits <m.nitschke@clearcode.cc>
2015-02-19 09:42:08 +0100

    changing name of repo and adding some tests

--- tests/services/Logger.spec.js
+++ tests/services/Logger.spec.js
@@ -3,25 +3,7 @@ describe('Unit: Logger', function () {
         loggerProvider,
         $log;
 
-    beforeEach(module('angular-cc-logger'),function($provide) {
-        //loggerServiceMocked = {};
-        //
-        //loggerServiceMocked.error = function (message) {
-        //    var indicator = 0;
-        //    if (!loggerProvider.debug) {
-        //        return;
-        //    }
-        //
-        //    for (var handler in loggerProvider.handlers) {
-        //        loggerProvider.handlers[handler](250, message);
-        //        indicator++;
-        //    }
-        //    $log.error(message);
-        //    return indicator;
-        //};
-        //
-        //$provide.value('angular-cc-logger.Logger.error', loggerServiceMocked.error);
-    });
+    beforeEach(module('angular-cc-logger'));
 
     /**
      * First mocked handler
@@ -29,7 +11,6 @@ describe('Unit: Logger', function () {
      * @param level
      * @param message
      */
-
     var test_1 = function(level, message){
         console.log('Created to check if test_1 runs properly');
     };
@@ -40,7 +21,6 @@ describe('Unit: Logger', function () {
      * @param level
      * @param message
      */
-
     var test_2 = function(level, message){
         console.info('Created to check if test_2 runs properly');
     };
@@ -48,7 +28,6 @@ describe('Unit: Logger', function () {
     /**
      * Injecting and creating mocked service and provider objects
      */
-
     beforeEach(inject(['angular-cc-logger.Logger', 'angular-cc-logger.$logger', '$log', function (Logger, provider, log) {
         loggerService = Logger;
         loggerProvider = provider;
@@ -58,13 +37,11 @@ describe('Unit: Logger', function () {
     /**
      * Tests
      */
-
     it('should be defined', shouldBeDefined);
 
     /**
      * Checking if mocked objects are defined
      */
-
     function shouldBeDefined(){
         expect(loggerService).toBeDefined();
         expect(loggerProvider).toBeDefined();
@@ -73,7 +50,6 @@ describe('Unit: Logger', function () {
     /**
      * Tests for Logger service
      */
-
     describe('testing each of Logger function', function(){
 
         /**
@@ -90,13 +66,13 @@ describe('Unit: Logger', function () {
 
             spyOn(console, 'log');
             spyOn(console, 'info');
-
         });
 
         /**
          * Tests
          */
         it('should be defined (handler)', shouldBeDefinedHandler);
+        it('should run logger with disabled debugger', shouldRunLoggerWithDisabledDebugger);
         it('should run logger error', shouldRunLoggerError);
         it('should run logger warning', shouldRunLoggerWarn);
         it('should run logger notice', shouldRunLoggerNotice);
@@ -110,6 +86,14 @@ describe('Unit: Logger', function () {
                 expect(loggerProvider.handlers[i]).toBeDefined();
                 expect(angular.isFunction(loggerProvider.handlers[i])).toBe(true);
             }
+        }
+
+        function shouldRunLoggerWithDisabledDebugger(){
+            loggerProvider.debug = false;
+
+            loggerService.error('error');
+            expect(console.log).not.toHaveBeenCalled();
+
         }
 
         /**
@@ -169,3 +153,24 @@ describe('Unit: Logger', function () {
         }
     });
 });
+
+
+//beforeEach(module('angular-cc-logger'),function($provide) {
+//    loggerServiceMocked = {};
+//
+//    loggerServiceMocked.error = function (message) {
+//        var indicator = 0;
+//        if (!loggerProvider.debug) {
+//            return;
+//        }
+//
+//        for (var handler in loggerProvider.handlers) {
+//            loggerProvider.handlers[handler](250, message);
+//            indicator++;
+//        }
+//        $log.error(message);
+//        return indicator;
+//    };
+//
+//    $provide.value('loggerService.error', loggerServiceMocked.error);
+//});
